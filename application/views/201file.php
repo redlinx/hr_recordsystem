bc5099a54d1cae5ef8fd9930c0c1ae5a03813ea9
ivankarlsombilla <ivansombilla@gmail.com>
2013-11-17 11:50:48 +0800

    layouts for 201files and PAASCU forms

---
+++ application/views/201file.php
@@ -0,0 +1,141 @@
+<html>
+    <title> 201 File </title>
+<body bgcolor="pink">
+    <p></p><center><b>University of the Immaculate Conception
+    <br/>Fr. Selga Street, Davao City
+    <br/><br/> FACULTY'S INFORMATION SHEET</b></center></p>
+    <br/>
+<center>
+	<form>
+
+		<fieldset style="width: 320px;">
+		<legend><b>Personal Information of Faculty:</b></legend>
+		First name: &nbsp&nbsp&nbsp <input type="text" name="Fname"><br />
+		Middle name: <input type="text" name="Mname"><br />
+		Last name: &nbsp&nbsp&nbsp <input type="text" name="Lname"><br />
+                Gender: &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<select><option>Select</option><option value="Male">Male</option><option value="Female">Female</option></select>
+                <br/>Religion: &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp<input type="text" name="Religion"><br />
+                Date of Birth:&nbsp<input type="text" name="bday"><br />
+                Place of Birth:&nbsp<input type="text" name="pday"><br />
+                City Address:<input type="text" name="address"><br />
+                Province Address:<input type="text" name="paddress"><br />
+                Telephone #:<input type="text" name="phone"><br />
+                Cellphone #:<input type="text" name="cp"><br />
+                Email Address:<input type="text" name="email"><br />
+                Civil Status: <select>
+                    <option>Select</option>
+                    <option value="single">Single</option>
+                    <option value="married">Married</option>
+                    <option value="hof">Head of the Family</option>
+                    <option value="ls">Legally Separated</option>
+                    <option value="ws">Working Spouse</option>
+                    <option value="nws">Non-Working Spouse</option>
+                    <option value="eeu">Entitled Exemption Units</option>
+                    <option value="neeu">Not Entitled to Exemption Unit</option>
+                </select>
+                </fieldset>
+        </form>
+   
+    
+        <form>
+            <fieldset style="width: 450px;">
+                <legend><b>Educational Attainment:</b></legend>
+                Grade School:<input type="text" name="gschool">&nbsp&nbsp Year<input type="text" name="gyear"><br/>
+                High School: <input type="text" name="hschool">&nbsp&nbsp&nbsp&nbsp Year<input type="text" name="hsyear"><br/>
+                College:<input type="text" name="cschool">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Year<input type="text" name="cschool"><br/>
+                M.A:<input type="text" name="maschool">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Year<input type="text" name="maschool"><br/>
+                Ph. D.:<input type="text" name="dschool">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Year<input type="text" name="dschool"><br/>
+                Other Sp. Study:<input type="text" name="osschool">Year<input type="text" name="oschool"><br/>
+            </fieldset>
+        </form>
+        
+        <form>
+           <fieldset style="width: 520px;">
+            <legend><b>Eligibilities:</b></legend>
+            Name of Exam &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Date/Place of Exam &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Rating<br/>
+            <input type="text" name="1stexam">&nbsp&nbsp&nbsp
+                <input type="text" name="1stdate">&nbsp&nbsp&nbsp
+                    <input type="text" name="1strating"><br/>
+            <input type="text" name="2ndexam">&nbsp&nbsp&nbsp
+                <input type="text" name="2nddate">&nbsp&nbsp&nbsp
+                    <input type="text" name="2ndrating"><br/>
+            <input type="text" name="3rdexam">&nbsp&nbsp&nbsp
+                <input type="text" name="3rddate">&nbsp&nbsp&nbsp
+                    <input type="text" name="3rdtrating"><br/>
+            Special Talents/Skills:<br/> <input type="text" name="3rdexam">&nbsp&nbsp&nbsp
+                <input type="text" name="3rddate">&nbsp&nbsp&nbsp
+                    <input type="text" name="3rdtrating"><br/>
+           </fieldset>
+        </form>
+        
+        <form>
+            <fieldset style="width: 520px;">
+                <legend><b>Work Experience:</b></legend>
+                Company/Address &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Position &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Inclusive Dates<br/>
+                <input type="text" name="1stcom">&nbsp&nbsp&nbsp
+                    <input type="text" name="1stpos">&nbsp&nbsp&nbsp
+                        <input type="text" name="1stID"><br/>
+                <input type="text" name="2ndcom">&nbsp&nbsp&nbsp
+                    <input type="text" name="2ndpos">&nbsp&nbsp&nbsp
+                        <input type="text" name="2ndID"><br/>
+                <input type="text" name="3rdcom">&nbsp&nbsp&nbsp
+                    <input type="text" name="3rdpos">&nbsp&nbsp&nbsp
+                        <input type="text" name="3rdID"><br/>
+                <input type="text" name="4thcom">&nbsp&nbsp&nbsp
+                    <input type="text" name="4thpos">&nbsp&nbsp&nbsp
+                        <input type="text" name="4thID"><br/>
+                <input type="text" name="1stcom">&nbsp&nbsp&nbsp
+                    <input type="text" name="5thpos">&nbsp&nbsp&nbsp
+                        <input type="text" name="5thtID"><br/>
+            </fieldset>
+        </form>
+        
+        <form>
+            <fieldset style="width: 720px;">
+            <center>Date employed in UIC:<br/></center>
+            Trainee: <input type="text" name="trainee">&nbsp&nbsp&nbsp
+            Probitionary:<input type="text" name="probitionary">&nbsp&nbsp&nbsp
+            Regular:  <input type="text" name="regular"><br/><br/>
+            SSS #:<input type="text" name="sss">&nbsp&nbsp&nbsp
+            Pag-ibig:<input type="text" name="pagibig">&nbsp&nbsp&nbsp
+            Philhealth:<input type="text" name="philhealth">&nbsp&nbsp&nbsp<br/>
+            TIN:<input type="text" name="tin"><br/>
+	    Name of Children: &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp Date of Birth:<br/>
+	    <input type="text" name="1stchild">&nbsp&nbsp&nbsp
+                        <input type="text" name="1stdbirth"><br/>
+	    <input type="text" name="2ndchild">&nbsp&nbsp&nbsp
+                        <input type="text" name="2nddbirth"><br/>
+	    <input type="text" name="3rdchild">&nbsp&nbsp&nbsp
+                        <input type="text" name="3rddbirth"><br/>
+	    <input type="text" name="4thchild">&nbsp&nbsp&nbsp
+                        <input type="text" name="4thdbirth"><br/>
+	    <input type="text" name="5thchild">&nbsp&nbsp&nbsp
+                        <input type="text" name="5thdbirth"><br/>
+	    <input type="text" name="6thchild">&nbsp&nbsp&nbsp
+                        <input type="text" name="6thdbirth"><br/>
+	    <input type="text" name="7thchild">&nbsp&nbsp&nbsp
+                        <input type="text" name="7thdbirth"><br/>    
+			    
+            </fieldset>
+        </form>
+	<form>
+	    <fieldset style="width: 520px;">
+	    <legend><b>Information about Spouse(Husband/Wife):</b></legend>
+	    Family Name:<input type="text" name="Lname"><br/>
+	    First Name:<input type="text" name="Fname"><br/>
+	    Middle Name:<input type="text" name="Mname"><br/>
+	    Date of Birth:<input type="text" name="DOB"><br/>
+	    Place of Brith:<input type="text" name="POB"><br/>
+	    Contact #:<input type="text" name="contact"><br/>
+	    Educational Attainment(highest Degree):<input type="text" name="EA"><br/>
+	    Present Occupation:<input type="text" name="PO"><br/>
+	    Employer/Address:<input type="text" name="EmA"><br/>
+	    SSS#:<input type="text" name="SSS"><br/>
+	    Pag-ibig:<input type="text" name="Pag-ibig"><br/>
+	    Philhealth#:<input type="text" name="PH"><br/>
+	    TIN:<input type="text" name="TIN"><br/>
+	    </fieldset>
+	</form>
+</center>
+</body>
+</html>
---
+++ application/views/paascu_profile.php
@@ -0,0 +1,260 @@
+<html>
+    <head>
+        <title>PAASCU Profile</title>
+    </head>
+    
+    <body bgcolor="pink">
+        <center><h1>FACULTY<br/>
+            <p>QUESTIONAIRE FOR INDIVIDUAL STAFF MEMBERS</h1><br/>
+            (This information for each staff member should be gathered and
+            subimitted to the evaluating team before it proceeds to fill out
+            the survey form for the faculty)<br/><br/>
+            NAME:<input type="text" name="name">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+            DEPARTMENT:<input type="text" name="department"><br/>
+            RANK:<input type="text" name="name">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+            SCHOOL:<input type="text" name="name"></p><br/></center>
+            
+            <center><p>A. ACADEMIC AND PROFESSIONAL PREPARATION</p>
+            <div>
+                <table id="APR" border="5" color="Blue">
+		    <th>Degrees</th>
+		    <th>Educational Institution</th>
+		    <th>Field of Specialization</th>
+                    <th>Date</th>
+                <tr>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                </tr>
+                <tr>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                </tr>
+                <tr>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                </tr>
+                <tr>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                </tr>
+                </table>
+            </div>
+            
+        <p>Special Training:<br/>
+        <textarea rows="4" cols="50">
+         </textarea>
+        </p>
+        
+        <p>B. EDUCATIONAL AND PROFESSIONAL TRAINING<br/>
+        1. Teaching Experiences</p>
+        <div>
+                <table id="APR" border="5">
+		    <th>Designation</th>
+		    <th>Institution</th>
+		    <th>Dates</th>
+                    <th>No. of<br/>Years</th>
+                <tr>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                </tr>
+                <tr>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                </tr>
+                <tr>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                </tr>
+                </table>
+            </div>
+        <br/><p>2. Professional Experience(other than teaching)</p>
+        <div>
+                <table id="PE" border="5">
+                    <th>Designation</th>
+                    <th>Institution</th>
+                    <th>Dates</th>
+                    <th>No.of<br/>Years</th>
+                <tr>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                </tr>
+                <tr>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                </tr>
+                <tr>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                </tr>
+                </table>
+        </div>
+        <br/>
+        <p><b>C. WEEKLY SCHEDULE</b><br/>
+        Indicate in the table below, classes and activities regularly assigned or carried out in each period.</p>
+        <div>
+            <table id="WS" border="5">
+                <th>Time</th>
+                <th>Room</th>
+                <th>Monday</th>
+                <th>Tuesday</th>
+                <th>Wednesday</th>
+                <th>Thursday</th>
+                <th>Friday</th>
+                <th>Saturday</th>
+            <tr>
+                <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+            </tr>
+            <tr>
+                <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+            </tr>
+            <tr>
+                <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+            </tr>
+            <tr>
+                <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+            </tr>
+            <tr>
+                <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+            </tr>
+            <tr>
+                <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+            </tr>
+            <tr>
+                <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+            </tr>
+            <tr>
+                <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+                    <td><input type="text" name="name"></td>
+            </tr>
+            </table>
+        </div>
+        
+            <p>Comments:<br/>
+        <textarea rows="8" cols="50" name="comments">
+         </textarea>
+        </p>
+        <p><b>D. PROFESSIONAL ACTIVITIES</b><br/>
+        1. Membership in Professioanl Organizations</p>
+        <table id="MPO" border="5">
+            <tr><td><input type="text" name="name"></td></tr>
+            <tr><td><input type="text" name="name"></td></tr>
+            <tr><td><input type="text" name="name"></td></tr>
+            <tr><td><input type="text" name="name"></td></tr>
+            <tr><td><input type="text" name="name"></td></tr>
+            <tr><td><input type="text" name="name"></td></tr>
+            </table>
+        <p>2. Professional Reading<br/>
+        List below the professional BOOKS which you have read within the last six months,<br/>
+        and the professional PERIODICALS you regularly read.</p>
+        <div>
+        <table id="MPO" border="5">
+            <tr><td><input type="text" name="name"></td></tr>
+            <tr><td><input type="text" name="name"></td></tr>
+            <tr><td><input type="text" name="name"></td></tr>
+            <tr><td><input type="text" name="name"></td></tr>
+            <tr><td><input type="text" name="name"></td></tr>
+            <tr><td><input type="text" name="name"></td></tr>
+            </table>
+        </div>
+        <p>3. In-Service Courses<br/>
+        Indicate courses taken during during the past THREE YEARS or NOW being taken.<br/>
+        DO NOT include courses taken BEFORE beginning to teach.</p>
+        <div>
+        <table id="ISC" border="5">
+            <tr><td><input type="text" name="name"></td></tr>
+            <tr><td><input type="text" name="name"></td></tr>
+            <tr><td><input type="text" name="name"></td></tr>
+            <tr><td><input type="text" name="name"></td></tr>
+            <tr><td><input type="text" name="name"></td></tr>
+            <tr><td><input type="text" name="name"></td></tr>
+            </table>
+        </div>
+        <p>4. Indicate research activities and/or publications completed in the past five years.</p>
+        <div>
+        <table id="research" border="5">
+            <tr><td><input type="text" name="name"></td></tr>
+            <tr><td><input type="text" name="name"></td></tr>
+            <tr><td><input type="text" name="name"></td></tr>
+            <tr><td><input type="text" name="name"></td></tr>
+            <tr><td><input type="text" name="name"></td></tr>
+            <tr><td><input type="text" name="name"></td></tr>
+            </table>
+        </div>
+          </center> 
+    </body>
+</html>
