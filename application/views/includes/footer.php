407680535d190976b3e31773ab55aa7276429172
jaspherintes <jazzintes06@gmail.com>
2014-03-24 18:09:29 +0800

    css js

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -3,6 +3,9 @@
 </div><!-- CLOSING SA CONTENT-->
 <!-- <p class="footer"> © Human Resource Office, University of the Immaculate Conception, Davao City, Philippines</p> -->
 </div>
+   <link href="<?php echo base_url().'assets/css/jquery.dataTables.css'?>" rel="stylesheet" type="text/css">
+<script src="<?php echo base_url('assets/js/jquery.dataTables.js') ?>" /></script>
+
 <script src="<?php echo base_url('assets/js/jquery-1.9.1.js') ?>" /></script>
 <script src="<?php echo base_url('assets/js/jquery-ui.js') ?>" /></script>
 <script> $(function() { $( "#accordion1" ).accordion({ collapsible: true, active: false }); }); </script>
@@ -10,5 +13,9 @@
 <script> $(function() { $( "#accordion2" ).accordion({ collapsible: true, active: false }); }); </script>
 <script> $(function() { $( "#promotions" ).accordion({ collapsible: true, active: false }); }); </script>
 <script> $(function() { $( "#faculty" ).accordion({ collapsible: true, active: false }); }); </script>
+<script> $(document).ready( function () {
+  var table = $('#example').DataTable();
+} );
+</script>
 </body>
 </html>
